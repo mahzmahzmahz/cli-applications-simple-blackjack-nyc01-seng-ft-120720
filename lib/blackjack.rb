fb41e7685a1f439d408382f11236db6babfb9388
Alex Marz <alexhmarz@gmail.com>
2020-11-24 23:02:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,9 +8,8 @@ def deal_card
  rand(1...11)
 end
 
-def display_card_total(card)
+def display_card_total(card_total)
   # code #display_card_total here
-  card_total = card.to_i + card.to_i
   puts "Your cards add up to #{card_total}"
 end
 
