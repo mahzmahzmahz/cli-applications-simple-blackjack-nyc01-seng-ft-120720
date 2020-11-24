3cf5b9c094bfcc9bc9289b74bd80fcf9453f0b87
Alex Marz <alexhmarz@gmail.com>
2020-11-24 23:08:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,7 +30,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_total = deal_card.to_i + deal_card.to_i 
+  card_total = deal_card.to_i + deal_card.to_i
+  puts display_card_total(card_total)
   
   
   
